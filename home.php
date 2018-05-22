3034619f2212cdc8f61c272024e4f77dd875d7de
AlexR <alexia_roman@orange.fr>
2018-05-22 16:44:07 +0200

    first import

---
+++ home.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"> 
+	<link rel="stylesheet" type="text/css" href="stylebanana.css"/>
+		<title>PRODUCTS</title>
+		
+	</head>
+	<body>
+
+	<h1> PRODUITS</h1>
+
+	<form action="search.php" method="GET">
+
+		<label>Rechercher</label>
+		<input type="text" name="search">
+		<button type="submit">envoi</button>
+
+	</form>
+
+
+		<?php 
+		foreach ($list as $id => $product) { 
+			echo "<li>";
+			echo "name"." : ";
+    		echo $product["name"]."</br>";
+
+    		echo "description"." : ";
+			echo $product["description"]."</br>";
+			
+			echo "price"." : ";
+			echo $product["price"]."€"."</br>";
+
+			echo "<img src='{$product["image"]}'>";
+    		echo "</li>";
+		}
+		?>
+			
+</body>
+</html>
+
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php  
+
+
+function getProductList() {
+	$user = 'root';
+	$password = 'troiswa';
+	$db = new PDO('mysql:host=localhost;dbname=bananastore', $user, $password);
+	$sql = "SELECT * FROM product";
+	$statement = $db->prepare($sql);
+	$statement->execute();
+	$products = $statement->fetchAll(\PDO::FETCH_ASSOC);
+	return $products;
+}
+
+
+$list = getProductList();
+
+
+include "home.php";
